1ec96f48dd9788fd8718c7c00b8c9bbb6aff4b7a
procir <maegya19@yahoo.co.jp>
2020-10-03 09:06:57 +0900

    バリデーションエラーと、検索エラー時の処理追加

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -10,7 +10,7 @@ class TwitterController extends Controller
 {
 	public function searchWord(Request $request)
 	{
-		$requested_words = $request->word;
+		$requested_words = $request->validate(['word' => 'required']);
 		$search_words = \Twitter::get('search/tweets', array('q' => $requested_words, 'count' => '1',  'result_type' => 'recent'))->statuses;
 		return view('result', compact('search_words'));
 	}
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
 @section('content')
+@if ($search_words)
 {{ $search_words[0]->text }}
+@else
+そのキーワードが含まれるツイートが見つかりません
+@endif
 @endsection
